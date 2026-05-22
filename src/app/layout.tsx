b9b3a42d3165a6f5f29ceb9550f008df0b8e4858
Subhanshu jha <138554397+subhanahujha007@users.noreply.github.com>
2026-05-22 11:32:17 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import Script from 'next/script';
 import { ConvexChildProvider } from "@/components/convex-provider";
 import {Toaster} from "sonner"
 import { EdgeStoreProvider } from "@/lib/edgestore";
@@ -34,12 +33,7 @@ storageKey="Jotion-change-theme">
   {children}</ThemeProvider>
   </EdgeStoreProvider>
 </ConvexChildProvider>
-<Script
-  src="http://localhost:4000/sdk/widget.js"
-  data-key="chatsop_dda09084900f187fc3f0d8a5bc488c806b72f24f3c49f42b"
-  data-api-base-url="http://localhost:4000"
-  strategy="afterInteractive"
-/>
+
       </body>
     </html>
   );
